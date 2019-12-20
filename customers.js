fa0d7eca5f91531f42876f7da3f3315d7705582d
lhazy111 <lhazy111@googlemail.com>
2019-12-20 14:29:53 +0000

    browsing customers added

--- customers.js
+++ customers.js
@@ -2,25 +2,61 @@ let formInput = document.getElementById('customers');
 let choice_New = document.getElementById("new_btn");
 let choice_Browse = document.getElementById("browse_btn");
 let choice_Delete = document.getElementById("delete_btn");
-let customers = [];
+let choice_prev = document.getElementById("prev_btn");
+let choice_next = document.getElementById("next_btn");
+let customersList = [];
+let customer_number = 1;
 formInput.addEventListener("submit", createNewCustomer);
 choice_New.addEventListener("click", addNewCustomer);
 choice_Browse.addEventListener("click", browse_Customers);
+choice_next.addEventListener("click", showCustomerNumberUp);
+choice_prev.addEventListener("click", showCustomerNumberDown);
+choice_Delete.addEventListener("click", deleteCustomer);
+
+function deleteCustomer(){
+    
+}
+
+function showCustomerNumberUp() {
+    if (customer_number + 1 > customersList.length) {
+        customer_number = 1;
+    } else {
+        customer_number++;
+    }
+    display_Customer();
+}
+
+function showCustomerNumberDown() {
+    if (customer_number === 1) {
+        customer_number = customersList.length
+    } else {
+        customer_number--;
+    }
+    display_Customer();
+}
+
 
 function addNewCustomer() {
-        document.getElementById('add_Customer').style.display = 'initial';
+    document.getElementById('browse_Customers').style.display = 'none';
+    document.getElementById('add_Customer').style.display = 'initial';
 }
 
 function browse_Customers() {
     document.getElementById('add_Customer').style.display = 'none';
-    if (localStorage.getItem("BobCustomers")){
-        customers = JSON.parse(localStorage.getItem("BobCustomers"))
+    if (localStorage.getItem("BobCustomers")) {
+        customersList = JSON.parse(localStorage.getItem("BobCustomers"))
     } else {
-        alert ("Customers base empty");
+        alert("Customers base empty");
     }
-
     document.getElementById("browse_Customers").style.display = 'initial';
+    display_Customer();
+}
 
+function display_Customer() {
+    document.getElementById("customer_number").innerHTML = customer_number + "/" + customersList.length;
+    document.getElementById("customer_name").innerHTML = customersList[customer_number - 1].c_name;
+    document.getElementById("customer_phone").innerHTML = customersList[customer_number - 1].c_phone;
+    document.getElementById("customer_notes").innerHTML = customersList[customer_number - 1].c_notes;
 }
 
 function createNewCustomer() {
@@ -33,32 +69,16 @@ function createNewCustomer() {
     alert('Customers name:' + newCustomerObject.c_name);
     alert('Customers phone:' + newCustomerObject.c_phone);
     alert('Customers notes:' + newCustomerObject.c_notes);
-
-    //document.getElementById('calc_result').style.display = 'initial';
-    //document.getElementById('litr').innerHTML = (newPaintCalcObject.w_width * newPaintCalcObject.w_height / newPaintCalcObject.coverage).toFixed(2) +' litres of paint';
-    //document.getElementById('buckets').innerHTML = (newPaintCalcObject.w_width * newPaintCalcObject.w_height / newPaintCalcObject.coverage / newPaintCalcObject.bucket_size).toFixed(2) +' buckets of paint';
-    //document.getElementById('all_price').innerHTML = 'Paint cost: ' + (newPaintCalcObject.w_width * newPaintCalcObject.w_height / newPaintCalcObject.coverage / newPaintCalcObject.bucket_size * newPaintCalcObject.bucket_price).toFixed(2) +' monies';
-
-
-
-    //}
-
-
-   if (localStorage.getItem("BobCustomers")) {
-    alert('dodanie');
-    let BobCustomers = JSON.parse(localStorage.getItem("BobCustomers"));
+    if (localStorage.getItem("BobCustomers")) {
+        alert('dodanie');
+        let BobCustomers = JSON.parse(localStorage.getItem("BobCustomers"));
         alert('dodanie1');
-        //alert(questionsStorage.question);
-        //alert(newQuestionObject.question);
         BobCustomers.push(newCustomerObject);
-        //alert('dodanie2');
         localStorage.setItem('BobCustomers', JSON.stringify(BobCustomers));
-        //alert('dodano?');
     } else {
         alert('pierwszy');
-       // alert(newQuestion.question);
-        localStorage.setItem("BobCustomers",  JSON.stringify([newCustomerObject]));
+        localStorage.setItem("BobCustomers", JSON.stringify([newCustomerObject]));
 
-}
-formInput.reset();
+    }
+    formInput.reset();
 }
